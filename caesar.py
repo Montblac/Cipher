0cba328f826c05af99c479bfb03a4dcde3cba0ac
Montblac <montblac95@gmail.com>
2019-01-14 23:13:33 -0800

    Added setKey

---
+++ caesar.py
@@ -0,0 +1,13 @@
+class CaesarCipher:
+    def __init__(self, key=0, text=None):
+        self.key = key
+        self.text = text
+
+    def setKey(self, key):
+        self.key = key
+        print("The cipher key has been set.")
+
+    def encrypt(self):
+        self.ke
+        if text != None:
+
