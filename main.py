7b416ae1d0d193973c7b415e1d2c4f5b36e0fa03
Montblac <montblac95@gmail.com>
2019-01-15 17:47:59 -0800

    Moved texts into a list

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import cipher
 import random
 
-text1 = "Facts are stubborn, but statistics are more pliable."
-text2 = "One should always play fairly when one has the winning cards."
-
-
 if __name__ == '__main__':
     ciph = cipher.Cipher()
-    keys = [0, 1, 5, 10, 20, 30, 60, 120]
-    texts = [text1, text2]
+
+    texts = [
+        "Facts are stubborn, but statistics are more pliable.",
+        "One should always play fairly when one has the winning cards."
+    ]
+    keys = [0, 1, 5, 10, 20, 30, 45, 51, 53, 60, 120]
 
     ciph.key = random.choice(keys)
     print("Key used:", ciph.key, "\n")
