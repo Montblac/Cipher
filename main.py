947b2b09cc8d1896177b63b133d4569ff68e12e0
Montblac <montblac95@gmail.com>
2019-01-15 17:44:20 -0800

    Improved checking to be more concise and remove clutter Added support for multiple key testing and display

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cipher
+import random
 
 text1 = "Facts are stubborn, but statistics are more pliable."
 text2 = "One should always play fairly when one has the winning cards."
@@ -6,21 +7,20 @@ text2 = "One should always play fairly when one has the winning cards."
 
 if __name__ == '__main__':
     ciph = cipher.Cipher()
-    ciph.key = 4
+    keys = [0, 1, 5, 10, 20, 30, 60, 120]
     texts = [text1, text2]
+
+    ciph.key = random.choice(keys)
+    print("Key used:", ciph.key, "\n")
 
     for text in texts:
         ciph.text = text
-        print("\n=== INITIAL MESSAGE ===")
-        print(text)
+        print("  Initial MSG: " + text)
 
-        print("\n=== ENCRYPTED MESSAGE ===")
         ciph.transform()
-        print(ciph.text)
+        print("Encrypted MSG: " + ciph.text)
 
-        print("\n=== DECRYPTED MESSAGE ===")
         ciph.toggle_mode()
         ciph.transform()
-        print(ciph.text)
-
-        print("\n=== END MESSAGE ===\n")
+        print("Decrypted MSG: " + ciph.text)
+        print("\n")
